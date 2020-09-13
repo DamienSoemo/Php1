d63476f72fe48c15bb676a0671da7cf7cbff8a82
dsoem <Soemo>
2020-09-13 20:02:44 +0200

    001- Beginnen met progammeren in PHP

---
+++ h2/Thomas Edison.php
@@ -0,0 +1,6 @@
+<?php
+$a = "Ik heb niet gefaald.";
+$b = " die niet werken.";
+$c = 1000;
+
+echo $a." Ik heb alleen ".$c." manieren gevonden".$b;
---
+++ h2/Tyra Banks.php
@@ -0,0 +1,6 @@
+<?php
+$a = "ry ";
+$b = ", but";
+$c = " fail";
+
+echo "T". $a. "and ". $c. $b. " never fail to try.";
