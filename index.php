10b4be6564ffb7c17798e1f1a261ffd087fe6d81
dsoem <Soemo>
2020-09-28 13:26:36 +0200

    001- Beginnen met progammeren in PHP

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
-echo 'Helloworld';
+$a = "Ik heb niet gefaald.";
+$b = " die niet werken.";
+$c = 1000;
 
+echo $a." Ik heb alleen ".$c." manieren gevonden".$b. "<br>";
+
+
+$a = "die geen fout";
+$b = "en";
+$c = "niet.";
+
+echo "Mensen ".$a. " maken, werken ". $c."<br>";
+
+$a = "ry ";
+$b = ", but";
+$c = " fail";
+
+echo "T". $a. "and ". $c. $b. " never fail to try."."<br>";
+
+$a = "\" omgaan ";
+$b = " met ";
+$c = " teleu";
+
+echo "De cursus ". $a. $b.$c."rstellingen\"kan vanavond helaas niet doorgaan."."<br>";
